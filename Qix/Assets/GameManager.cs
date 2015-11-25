20191f8a63cae45367d36730d38e11acba4cd760
Dunnings <mrdaviddunnings@gmail.com>
2015-11-25 13:02:50 +0000

    Added Vectrosity

---
+++ Qix/Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Vectrosity;
+
+// Created by David Dunnings
+
+public class GameManager : MonoBehaviour {
+
+	void Awake(){
+		
+	}
+
+	void Start () {
+        VectorLine.SetLine(Color.green, new Vector2(0, 0), new Vector2(Screen.width - 1, Screen.height - 1));
+    }
+	
+	void Update () {
+		
+	}
+	
+	void OnDestroy(){
+		
+	}
+}
