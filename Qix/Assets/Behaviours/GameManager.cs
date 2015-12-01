a6aa42f41f6a3dbbdbf655064d5ac4394e4834c9
Daniel Weston <westondaniel93@gmail.com>
2015-12-01 15:54:48 +0000

    Updated main with game over

--- Qix/Assets/Behaviours/GameManager.cs
+++ Qix/Assets/Behaviours/GameManager.cs
@@ -188,18 +188,6 @@ public class GameManager : MonoBehaviour
                     //set winner text
                     winnerText.GetComponent<Text>().text = "Player " + winner + " Wins!";
 
-                    //for (int i = 0; i < playerUIElements.Count-1; i ++ )
-                    //{
-                    //    if (playerUIElements[i].activeSelf)
-                    //    {
-                    //        get UI element color
-                    //        Color get = playerUIElements[i].GetComponentInChildren<Image>().color;
-                    //        turn up alpha
-                    //        get.a = 0.5f;
-                    //        set UI element to full alpha
-                    //        playerUIElements[i].GetComponentInChildren<Image>().color = get;
-                    //    }
-                    //}
 
                     
 
@@ -221,6 +209,20 @@ public class GameManager : MonoBehaviour
                         if(player.activeSelf)
                         {
                             player.GetComponent<CharMovement>().joined = false;
+                        }
+                    }
+
+
+                    for (int i = 0; i < playerUIElements.Count - 1; i++)
+                    {
+                        if (playerUIElements[i].activeSelf)
+                        {
+                            //get UI element color
+                            Color get = playerUIElements[i].GetComponentInChildren<Image>().color;
+                            //turn up alpha
+                            get.a = 0.5f;
+                            //set UI element to full alpha
+                            playerUIElements[i].GetComponentInChildren<Image>().color = get;
                         }
                     }
 
